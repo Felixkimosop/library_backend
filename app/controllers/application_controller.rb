ede022d048a825a97b77406c80c5ae209511c818
osteen <osteen.mutinda@student.moringaschool.com>
2023-03-29 21:58:58 +0300

    routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,6 +27,7 @@ class ApplicationController < ActionController::Base
         if decoded_token
           user_id = decoded_token[0]['user_id']
           @user = User.find_by(id: user_id)
+     
         end
       end
     
@@ -41,3 +42,59 @@ class ApplicationController < ActionController::Base
 
 
 end
+# class ApplicationController < ActionController::Base
+#   protect_from_forgery with: :null_session
+#   before_action :authorize
+
+#   def encode_token(payload)
+#     # should store secret in env variable
+#     JWT.encode(payload, 'my_s3cr3t')
+#   end
+
+#   def auth_header
+#     # { Authorization: 'Bearer <token>' }
+#     request.headers['Authorization']
+#   end
+
+#   def decoded_token
+#     if auth_header
+#       token = auth_header.split(' ')[1]
+#       # header: { 'Authorization': 'Bearer <token>' }
+#       begin
+#         JWT.decode(token, 'my_s3cr3t', true, algorithm: 'HS256')
+#       rescue JWT::DecodeError
+#         nil
+#       end
+#     end
+#   end
+
+#   def current_user
+#     if decoded_token
+#       user_id = decoded_token[0]['user_id']
+#       @user = User.find_by(id: user_id)
+#     end
+#   end
+
+#   def current_admin
+#     if decoded_token
+#       admin_id = decoded_token[0]['admin_id']
+#       @admin = Admin.find_by(id: admin_id)
+#     end
+#   end
+
+#   def logged_in?
+#     !!current_user 
+#   end
+
+#   def authorized_user
+#     render json: { message: 'Please log in as a user' }, status: :unauthorized unless logged_in? && !!current_user
+#   end
+
+#   def authorized_admin
+#     render json: { message: 'Please log in as an admin' }, status: :unauthorized unless logged_in? && !!current_admin
+#   end
+
+#   def authorize
+#     authorized_user || authorized_admin
+#   end
+# end
