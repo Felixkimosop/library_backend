d9bebb2c37bf65fcb1d371328909b952462eb44b
osteen <osteen.mutinda@student.moringaschool.com>
2023-03-30 16:59:56 +0300

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   delete '/logout', to: 'session#destroy' 
   get '/loggedin', to: 'application#logged'
   get '/logged', to: 'application#administration'
+  delete 'logout', to: 'sessions#destroy'
+
   # get '/loggedin', to: 'users#current_user'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
