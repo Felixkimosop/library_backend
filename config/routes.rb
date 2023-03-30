693a6126323766a55f06cc71453b0333b4222a70
+ <+>
2023-03-30 17:15:03 +0300

    Modified

--- config/routes.rb
+++ config/routes.rb
@@ -7,14 +7,13 @@ Rails.application.routes.draw do
   resources :admins, only: [:index, :create, :show]
 
   post '/users/:id/add_book', to: 'users#add_book'
- 
+
   post '/login' , to: 'session#create'
   delete '/logout', to: 'session#destroy' 
   get '/loggedin', to: 'application#logged'
+  # get '/loggedin', to: 'users#current_user'
   get '/logged', to: 'application#administration'
   delete 'logout', to: 'sessions#destroy'
-
-  # get '/loggedin', to: 'users#current_user'
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
